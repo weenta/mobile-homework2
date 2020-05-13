ea03fb39c0227ab8210b74abe74b0694a5da4ffd
weenta <weenta@qq.com>
2020-05-14 07:46:17 +0800

    update

--- app/src/main/java/com/example/homework/BannerFragment.kt
+++ app/src/main/java/com/example/homework/BannerFragment.kt
@@ -2,11 +2,11 @@ package com.example.homework
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_banner.*
 
 /**
@@ -18,15 +18,22 @@ class BannerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_banner, container,false)
-
+        return inflater.inflate(R.layout.fragment_banner, container, false)
     }
 
     override fun onStart() {
         super.onStart()
-        investment.setOnClickListener(){
-            val intent = Intent(activity,InvestmentActivity::class.java)
-            startActivityForResult(intent,10000)
+        investment.setOnClickListener() {
+            val intent = Intent(activity, InvestmentActivity::class.java)
+            startActivityForResult(intent, 10000)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 10000 && resultCode == 20000) {
+            val investmentMsg = "Get investment " + data?.getStringExtra("investment")
+            Toast.makeText(activity, investmentMsg, Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/homework/InvestmentActivity.kt
+++ app/src/main/java/com/example/homework/InvestmentActivity.kt
@@ -1,12 +1,23 @@
 package com.example.homework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_investment.*
 
 class InvestmentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_investment)
+
+        btn_invest.setOnClickListener(){
+            val intent = Intent()
+            intent.putExtra("investment","1 million")
+            setResult(20000,intent)
+            finish()
+        }
     }
+
+
 }
